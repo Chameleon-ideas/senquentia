a4739563607096623f7ab3d487a8284a1eba6901
SyedShaheer-2001 <shaheer@chameleon-ideas.com>
2026-05-07 05:01:00 +0500

    sanity issue vercel

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { schemaTypes } from './src/sanity/schemaTypes'
+import { structure } from './src/sanity/structure'
 
 export default defineConfig({
   projectId: 'egq490jl',
   dataset: 'production',
   basePath: '/studio',
   plugins: [
-    structureTool()
+    structureTool({
+      structure: structure
+    })
   ],
   schema: {
     types: schemaTypes,
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -4,5 +4,56 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
     .items([
-      ...S.documentTypeListItems()
+      // SINGLETONS
+      S.listItem()
+        .title('Site Settings')
+        .id('siteSettings')
+        .child(S.document().schemaType('siteSettings').documentId('siteSettings')),
+      S.listItem()
+        .title('Home Page')
+        .id('homePage')
+        .child(S.document().schemaType('homePage').documentId('homePage')),
+      S.listItem()
+        .title('Ethos Page')
+        .id('aboutPage')
+        .child(S.document().schemaType('aboutPage').documentId('aboutPage')),
+      S.listItem()
+        .title('Services Page')
+        .id('servicesPage')
+        .child(S.document().schemaType('servicesPage').documentId('servicesPage')),
+      S.listItem()
+        .title('Journal Page')
+        .id('blogPage')
+        .child(S.document().schemaType('blogPage').documentId('blogPage')),
+      S.listItem()
+        .title('Contact Page')
+        .id('contactPage')
+        .child(S.document().schemaType('contactPage').documentId('contactPage')),
+      S.listItem()
+        .title('Footer')
+        .id('footer')
+        .child(S.document().schemaType('footer').documentId('footer')),
+
+      S.divider(),
+
+      // COLLECTIONS
+      ...S.documentTypeListItems().filter(
+        (listItem) =>
+          ![
+            'siteSettings',
+            'homePage',
+            'servicesPage',
+            'aboutPage',
+            'blogPage',
+            'contactPage',
+            'footer',
+            'faqPage',
+            'portfolioPage',
+            'testimonialsPage',
+            'localeString',
+            'localeText',
+            'localeBlock',
+            'media.tag',
+          ].includes(listItem.getId() || '')
+      ),
     ]);
