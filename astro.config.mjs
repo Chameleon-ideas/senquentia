d524d37161ca0990bba28893828bed40f6556c26
tuahamansoor <tuaha@chameleon-ideas.com>
2026-05-07 05:16:30 +0500

    fix: switch output to hybrid to enable Sanity Studio SSR on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from '@astrojs/vercel';
 import sanity from '@sanity/astro';
 
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel(),
   integrations: [
     react(),
